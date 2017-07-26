430bf918223f6cb6cba452b0a4346d0882256b76
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-07-26 12:38:50 +0200

    Validate token_version

--- lib/jwt/auth/token.rb
+++ lib/jwt/auth/token.rb
@@ -10,21 +10,26 @@ module JWT
     # In-memory representation of JWT
     #
     class Token
-      attr_accessor :expiration, :subject
+      attr_accessor :expiration, :subject, :token_version
 
       def valid?
-        !subject.nil? && !expiration.nil? && Time.at(expiration).future?
+        return false if subject.nil? || expiration.nil? || token_version.nil?
+        return false if Time.at(expiration).past?
+        return false if token_version != subject.token_version
+
+        true
       end
 
       def renew!
         self.expiration = nil
+        self.token_version = nil
       end
 
       def to_jwt
         payload = {
           :exp => expiration || JWT::Auth.token_lifetime.from_now.to_i,
           :sub => subject.id,
-          :ver => subject.token_version
+          :ver => token_version || subject.token_version
         }
         JWT.encode payload, JWT::Auth.secret
       end
@@ -41,6 +46,7 @@ module JWT
 
         token = JWT::Auth::Token.new
         token.expiration = payload['exp']
+        token.token_version = payload['ver']
 
         find_method = JWT::Auth.model.respond_to?(:find_by_token) ? :find_by_token : :find_by
         token.subject = JWT::Auth.model.send find_method, :id => payload['sub'], :token_version => payload['ver']
