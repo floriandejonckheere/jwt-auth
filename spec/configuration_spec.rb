21f205d71045624619787d9ec59eaea60664190c
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-07-26 14:04:01 +0200

    Update configuration spec

--- spec/configuration_spec.rb
+++ spec/configuration_spec.rb
@@ -1,16 +1,18 @@
 # frozen_string_literal: true
 
-require 'support/dummy_user'
+require 'securerandom'
+
+require 'rails_helper'
 
 RSpec.describe JWT::Auth do
   it 'configures correctly' do
     JWT::Auth.configure do |config|
-      config.token_lifetime = 666
+      config.token_lifetime = 24.hours
       config.secret = 'mysecret'
     end
 
-    expect(subject.token_lifetime).to eq 666
+    expect(subject.token_lifetime).to eq 24.hours
     expect(subject.secret).to eq 'mysecret'
-    expect(subject.model).to eq DummyUser
+    expect(subject.model).to eq User
   end
 end
