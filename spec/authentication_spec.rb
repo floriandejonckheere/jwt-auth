db2ef6078e9a59e496f7ae22f5060d74b2538089
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-07-26 13:33:15 +0200

    Add JWT::Authentication specs

---
+++ spec/authentication_spec.rb
@@ -0,0 +1,77 @@
+# frozen_string_listeral: true
+
+require 'rails_helper'
+
+RSpec.describe AuthenticationController, :type => :request do
+  let(:user) { User.create :activated => true }
+
+  let(:headers) do
+    {
+      'Authorization' => "Bearer #{JWT::Auth::Token.from_user(user).to_jwt}"
+    }
+  end
+
+  describe 'GET /public' do
+    context 'activated user' do
+      it 'is accessible without token' do
+        get '/public'
+
+        expect(response.status).to eq 204
+      end
+
+      it 'is accessible with token' do
+        get '/public', :headers => headers
+
+        expect(response.status).to eq 204
+      end
+    end
+
+    context 'disabled user' do
+      let(:user) { User.new }
+
+      it 'is accessible without token' do
+        get '/public'
+
+        expect(response.status).to eq 204
+      end
+
+      it 'is accessible with token' do
+        get '/public', :headers => headers
+
+        expect(response.status).to eq 204
+      end
+    end
+  end
+
+  describe 'GET /private' do
+    context 'activated user' do
+      it 'is not accessible without token' do
+        get '/private'
+
+        expect(response.status).to eq 401
+      end
+
+      it 'is accessible with token' do
+        get '/private', :headers => headers
+
+        expect(response.status).to eq 204
+      end
+    end
+
+    context 'disabled user' do
+      let(:user) { User.new }
+
+      it 'is not accessible without token' do
+        get '/private'
+
+        expect(response.status).to eq 401
+      end
+
+      it 'is not accessible with token' do
+        get '/private', :headers => headers
+
+        expect(response.status).to eq 401
+      end
+    end
+  end
+end
