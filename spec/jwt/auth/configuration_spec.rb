cd21cd0ae395d7435cc3bd32fa415948b314775c
Florian Dejonckheere <florian@floriandejonckheere.be>
2019-02-21 19:33:09 +0100

    Rewrite configuration spec

--- spec/jwt/auth/configuration_spec.rb
+++ spec/jwt/auth/configuration_spec.rb
@@ -5,16 +5,29 @@ require 'securerandom'
 require 'rails_helper'
 
 RSpec.describe JWT::Auth do
-  it 'configures correctly' do
-    JWT::Auth.configure do |config|
-      config.refresh_token_lifetime = 1.year
-      config.access_token_lifetime = 2.hours
-      config.secret = 'mysecret'
-    end
-
-    expect(subject.refresh_token_lifetime).to eq 1.year
-    expect(subject.access_token_lifetime).to eq 2.hours
-    expect(subject.secret).to eq 'mysecret'
-    expect(subject.model).to eq 'User'
+  ##
+  # Configuration
+  #
+  JWT::Auth.configure do |config|
+    config.refresh_token_lifetime = 1.year
+    config.access_token_lifetime = 2.hours
+    config.secret = 'mysecret'
   end
+
+  ##
+  # Test variables
+  #
+
+  # Ensure the User model is autoloaded, which initializes the JWT::Auth.model configuration entry
+  before { User }
+
+  ##
+  # Subject
+  #
+  subject { JWT::Auth }
+
+  ##
+  # Tests
+  #
+  it { is_expected.to have_attributes :refresh_token_lifetime => 1.year, :access_token_lifetime => 2.hours, :secret => 'mysecret', :model => 'User' }
 end
