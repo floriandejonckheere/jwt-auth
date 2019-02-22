2f9cb1940167f0d23baefc7dd713a59d30018193
Florian Dejonckheere <florian@floriandejonckheere.be>
2019-02-22 15:12:57 +0100

    Require Shoulda matchers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,7 @@ abort('The Rails environment is running in production mode!') if Rails.env.produ
 require 'spec_helper'
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
+require 'shoulda/matchers'
 
 require 'dummy/config/environment'
 # ENV['RAILS_ROOT'] ||= File.join File.dirname(__FILE__), 'spec', 'dummy'
